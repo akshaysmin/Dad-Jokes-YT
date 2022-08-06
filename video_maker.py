9968f59423729cebd6a4e1bed0bae7d91ac04813
unknown <akshaysmin@gmail.com>
2022-08-06 16:04:43 +0530

    clip made with no error but not ok

--- video_maker.py
+++ video_maker.py
@@ -1,2 +1,11 @@
 import moviepy.editor as mpy
-import gizeh as gz
+from PIL import Image
+import numpy as np
+
+def get_frame(x):
+	return img
+
+p = Image.open('Dark_mode_image.jpg')
+img = np.asarray(p)
+
+mpy.VideoClip(get_frame, duration=10).write_videofile('test.mp4', fps=12)
