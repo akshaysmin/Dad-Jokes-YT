fda57ca093d7e70dea60f03a4c4e8cef5457e85a
unknown <akshaysmin@gmail.com>
2022-08-06 10:30:01 +0530

    based joke validity on newness and sentence length

--- dadscraper.py
+++ dadscraper.py
@@ -6,43 +6,71 @@ import re
 ###CONSTANTS###
 regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
 MAX_JOKES = 10
+MAX_ITER = 10
+MAX_SENTENCES = 9
 
 ###FUNCTIONS###
-def is_joke_valid(jk):
+def is_joke_valid(jk, id):
+	sentences = [s if len(s)>1 for s in jk.split('.')]
 	if_no_links = len(re.findall(regex, jk))==0
-	return if_no_links
+	if_new_joke = id not in old_jokes
+	if_not_lengthy = len(sentences)<=MAX_SENTENCES
+	print(if_new_joke)
+	return if_no_links and if_new_joke
 
 with open('SECRET') as f:
 	reddit = eval(f.read())
 
-valid_jokes = 0
-ids = []
+#get id of old jokes
+old_jokes = []
+with open('dadjokes.csv') as f:
+	reader = csv.reader(f)
+	next(reader) #skips header
+	for row in reader:
+		old_jokes.append(row[0])
 
-while valid_jokes<MAX_JOKES:
-	submissions = reddit.subreddit("dadjokes").top(time_filter='day',
-                                                       limit=2*MAX_JOKES-valid_jokes+1)
-	#input('hehe')
-	with open('dadjokes.csv', 'a', newline='') as csvfile:
-		writer = csv.writer(csvfile, delimiter=',',)
-		for p in submissions:
-			title = p.title
-			text = p.selftext
-			id = p.id
-			
-			#speedbreaker
-			if  valid_jokes==MAX_JOKES:
-				break
-			elif (id in ids):
-				continue
-			elif is_joke_valid(title+text):
-				valid_jokes += 1
-			else:
-				continue
+def main():
+	valid_jokes = 0
+	ids = []
+	iter = 0
+	while valid_jokes<MAX_JOKES and iter<MAX_ITER:
+		iter += 1
+		submissions = reddit.subreddit("dadjokes").top(time_filter='day',
+	                                                       limit=iter*MAX_JOKES)
+		#input('hehe')
+		with open('dadjokes.csv', 'a', newline='') as csvfile:
+			writer = csv.writer(csvfile, delimiter=',',)
+			for p in submissions:
+				title = p.title
+				text = p.selftext
+				id = p.id
+				
+				#speedbreaker
+				if  valid_jokes==MAX_JOKES:
+					break
+				elif (id in ids):
+					continue
+				elif is_joke_valid(title+text, id):
+					valid_jokes += 1
+				else:
+					continue
+	
+				time = p.created
+				timestamp =  datetime.fromtimestamp(time).strftime("%Y/%m/%d, %H:%M:%S")
+				print(valid_jokes)
+				print(id)
+				print(title)
+				print(text)
 
-			ids.append(id)
-			time = p.created
-			timestamp =  datetime.fromtimestamp(time).strftime("%Y/%m/%d, %H:%M:%S")
-			print(valid_jokes)
-			print(id)
-			print(text)
-			writer.writerow([p.id, p.author, timestamp, p.url, p.title, p.selftext])
+				#save joke
+				try :
+					writer.writerow([id, p.author, timestamp, p.url, title, text])
+				except UnicodeEncodeError as err:
+					valid_jokes -= 1
+					print(err, 'Handled')
+					continue
+
+				ids.append(id)
+				print('NICE ONE')
+
+main()
