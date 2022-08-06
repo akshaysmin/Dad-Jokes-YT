69ab9b4c05cad2583e4795ef6024dab3d27c3d96
unknown <akshaysmin@gmail.com>
2022-08-06 22:29:57 +0530

    changing python from 3.10.6->3.9.13 solved graphics problems

--- video_maker.py
+++ video_maker.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 def get_frame(x):
 	return img
 
-p = Image.open('Dark_mode_image.jpg')
+p = Image.open('blue_dark_cloud.jpg')
 img = np.asarray(p)
 
 mpy.VideoClip(get_frame, duration=10).write_videofile('test.mp4', fps=12)
