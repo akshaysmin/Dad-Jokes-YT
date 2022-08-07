94623efffdec9fcdfc4997dd46e0c4bdcf6def77
unknown <akshaysmin@gmail.com>
2022-08-07 12:57:29 +0530

    one joke per second success

--- video_maker.py
+++ video_maker.py
@@ -6,12 +6,15 @@ import csv
 import textwrap
 
 class GetFrame:
-	def __init__(self, frames):
+	def __init__(self, frames, frame_rate = 1):
 		self.frames = frames
+		self.frame_rate = frame_rate
 
 	def __call__(self, x):
-		print(f'recieved {x}')
-		return self.frames[-1]
+		try:
+			return self.frames[int(self.frame_rate*x)]
+		except IndexError as err:
+			return self.frames[-1]
 
 def load_joke10():
 	joke10 = []
@@ -39,19 +42,20 @@ def main():
 	font_thickness = 2
 	
 	joke10 = load_joke10()
-	text = ''
 	frames = []
-	for jk in joke10:
-		text += jk[0] + '\n' + jk[1]
+	for i,jk in enumerate(joke10):
+		text = str(i) + '\n' + jk[0] + '\n' + jk[1]
 		wrapped_text = textwrap.wrap(text, width=int(width*0.1))
-	
+		image = background.copy()
+
 		for i, line in enumerate(wrapped_text):
 			textsize = cv2.getTextSize(line, font, font_size, font_thickness)[0]
 			gap = textsize[1] + 10
 			y = (height + textsize[1]) // 2 + i * gap
 			x = (width - textsize[0]) // 2
 			org = (x,y)
-			image = cv2.putText(background , line, org, font, font_size, font_color, font_thickness, cv2.LINE_AA)
+			image = cv2.putText( image , line, org, font, font_size, font_color, font_thickness, cv2.LINE_AA)
+
 		plt.imshow(image)
 		plt.show()
 #		cv2.imshow('hehe', image)
@@ -59,7 +63,7 @@ def main():
 		np_img = np.asarray(image)
 		frames.append(np_img)
 
-	get_frame = GetFrame(frames)
-	mpy.VideoClip(get_frame, duration=10).write_videofile('test.mp4', fps=12)
+	get_frame = GetFrame(frames, frame_rate=1)
+	mpy.VideoClip(get_frame, duration=11).write_videofile('test.mp4', fps=12)
 
 main()
