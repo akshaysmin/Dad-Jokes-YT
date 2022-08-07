193ec9669fadf6c0feffaba7d0c9484a3920fdac
unknown <akshaysmin@gmail.com>
2022-08-07 12:02:06 +0530

    added text wrap

--- video_maker.py
+++ video_maker.py
@@ -3,6 +3,7 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 import csv
+import textwrap
 
 class GetFrame:
 	def __init__(self, frames):
@@ -10,7 +11,7 @@ class GetFrame:
 
 	def __call__(self, x):
 		print(f'recieved {x}')
-		return self.frames[0]
+		return self.frames[-1]
 
 def load_joke10():
 	joke10 = []
@@ -24,26 +25,40 @@ def load_joke10():
 	
 
 def main():
+	global background, npimg, joke10, frames, wrapped_text
+
 	path = 'blue_dark_cloud.jpg'
 	background = cv2.imread(path)
 	background = cv2.cvtColor(background , cv2.COLOR_BGR2RGB)
+	height, width, channel = background.shape
 	
 	font = cv2.FONT_HERSHEY_SIMPLEX
 	org = (50, 50)
-	fontScale = 1
-	color = (255, 0, 0)
-	thickness = 2
+	font_size = 1
+	font_color = (255, 255, 255)
+	font_thickness = 2
 	
 	joke10 = load_joke10()
 	text = ''
 	frames = []
 	for jk in joke10:
 		text += jk[0] + '\n' + jk[1]
+		wrapped_text = textwrap.wrap(text, width=int(width*0.1))
 	
-		image = cv2.putText(background , text, org, font, fontScale, color, thickness, cv2.LINE_AA)
+		for i, line in enumerate(wrapped_text):
+			textsize = cv2.getTextSize(line, font, font_size, font_thickness)[0]
+			gap = textsize[1] + 10
+			y = (height + textsize[1]) // 2 + i * gap
+			x = (width - textsize[0]) // 2
+			org = (x,y)
+			image = cv2.putText(background , line, org, font, font_size, font_color, font_thickness, cv2.LINE_AA)
+		plt.imshow(image)
+		plt.show()
+#		cv2.imshow('hehe', image)
+#		cv2.waitKey(0)
 		np_img = np.asarray(image)
 		frames.append(np_img)
-	
+
 	get_frame = GetFrame(frames)
 	mpy.VideoClip(get_frame, duration=10).write_videofile('test.mp4', fps=12)
 
