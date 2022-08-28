41bdf4cad546b350180af780e3ff32547e03a1c4
unknown <akshaysmin@gmail.com>
2022-08-28 23:12:32 +0530

    spoken video success

--- audio_maker.py
+++ audio_maker.py
@@ -1,7 +1,7 @@
 # from TTS.config import load_config
 from TTS.utils.manage import ModelManager
 from TTS.utils.synthesizer import Synthesizer
-# import tempfile
+import tempfile
 
 manager = ModelManager()
 MODEL_NAMES = manager.list_tts_models()
@@ -10,32 +10,51 @@ MODEL_NAMES = manager.list_tts_models()
 # filters = ["vctk", "your_tts", "ek1"]
 # MODEL_NAMES = [model_name for model_name in MODEL_NAMES if not any(f in model_name for f in filters)]
 
-joke = "If a serial killer is chasing you......	you're both running for your life."
 
-speaker_idx = ""
+def tell_jokes(joke_list, model_name="en/ljspeech/glow-tts", speaker_idx="", delete_outfile=True):
+    '''
+    Input : list of sentences
+    Output : list of tempfiles of speeches
+    '''
+    global wavs, synthesizer
 
-# model info
-model_name = "en/ljspeech/glow-tts"
-# model_path = "C:\\Users\\user\\AppData\\Local\\tts\\tts_models--en--ljspeech--glow-tts\\model_file.pth"
-# config_path = "C:\\Users\\user\\AppData\\Local\\tts\\tts_models--en--ljspeech--glow-tts\\config.json"
-# model_item = manager.models_dict['tts_models']['en']['ljspeech']['glow-tts']
-model_path, config_path, model_item = manager.download_model(
-    f"tts_models/{model_name}")
+    speeches = []
+    wavs = []
 
-vocoder_name = model_item['default_vocoder']
-vocoder_path, vocoder_config_path, _ = manager.download_model(vocoder_name)
+    for joke in joke_list:
+        # model info
+        model_path, config_path, model_item = manager.download_model(
+            f"tts_models/{model_name}")
 
-print(f'joke : {joke}\nmodel_name : {model_name}')
-print(
-    f'model_name : \n{model_name}\n model_path : \n{model_path}\nconfig_path : \n{config_path}\nmodel_item : {model_item}')
+        vocoder_name = model_item['default_vocoder']
+        vocoder_path, vocoder_config_path, _ = manager.download_model(
+            vocoder_name)
 
-synthesizer = Synthesizer(
-    model_path, config_path, None, None, vocoder_path, vocoder_config_path,
-)
+        print(f'joke : {joke}\nmodel_name : {model_name}')
+        print(
+            f'model_name : \n{model_name}\n model_path : \n{model_path}\nconfig_path : \n{config_path}\nmodel_item : {model_item}')
 
-wavs = synthesizer.tts(joke)
+        synthesizer = Synthesizer(
+            model_path, config_path, None, None, vocoder_path, vocoder_config_path,
+        )
 
-with open('test.mp4', 'wb') as fp:
-    synthesizer.save_wav(wavs, fp)
+        wav = synthesizer.tts(joke, speaker_idx)
+        wavs.append(wav)
 
-# just a line
+        if delete_outfile == False:
+            with tempfile.NamedTemporaryFile(suffix=".wav", delete=delete_outfile) as fp:
+                synthesizer.save_wav(wavs, fp)
+                speeches.append(fp.name)
+
+    if delete_outfile == False:
+        return speeches
+    else:
+        return wavs, synthesizer
+
+
+if __name__ == '__main__':
+    wavs = 0
+    jokes = [
+        "If a serial killer is chasing you......	you're both running for your life."]
+    speeches = tell_jokes(jokes, delete_outfile=False)
+    print(f'file saved at : {speeches}')
--- video_maker.py
+++ video_maker.py
@@ -5,85 +5,132 @@ import matplotlib.pyplot as plt
 import csv
 import textwrap
 
+from audio_maker import tell_jokes
+import tempfile
+
 ###CONSTANTS##
 DURATION = 60
 BACKGROUND_IMG = 'blue_dark_cloud.jpg'
-
-
+mpy.AudioClip
 
 ###FUNCTIONS###
 
-class GetFrame:
-	def __init__(self, frames, frame_rate = None, duration=None):
-		self.frames = frames
-		self.duration = duration
-		if frame_rate is not None:
-			self.frame_rate = frame_rate
-		elif duration is not None:
-			self.frame_rate = len(frames)/duration
 
-	def __call__(self, x):
-		try:
-			return self.frames[int(self.frame_rate*x)]
-		except IndexError as err:
-			return self.frames[-1]
+class GetFrame:
+    def __init__(self, frames, durations=None, frame_rate=None, duration=None):
+        self.frames = frames
+        self.duration = duration
+        if durations is not None:
+            self.durations = durations
+        elif frame_rate is not None:
+            self.frame_rate = frame_rate
+        elif duration is not None:
+            self.frame_rate = len(frames)/duration
+
+    def __call__(self, x):
+        try:
+            if self.durations:
+                if x > sum(self.durations):
+                    raise IndexError
+                lower = 0
+                cum = 0
+                for i, dur in enumerate(self.durations):
+                    cum += dur
+                    if cum > x:
+                        upper = cum
+                        break
+                    elif cum < x:
+                        lower = cum
+
+                frame = self.frames[i]
+            else:
+                frame = self.frames[int(self.frame_rate*x)]
+        except IndexError as err:
+            return self.frames[-1]
+
+        return frame
+
 
 def load_joke10():
-	joke10 = []
-	with open('joke10.csv') as f:
-		reader = csv.reader(f)
-		for row in reader:
-			joke = (row[4],row[5])
-			joke10.append(joke)
-			next(reader)
-	return joke10
-	
+    joke10 = []
+    with open('joke10.csv') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            joke = (row[4], row[5])
+            joke10.append(joke)
+            next(reader)
+    return joke10
 
-def get_video():
-	global background, npimg, joke10, frames, text, wrapped_text
-	global font, org, font_size, font_color, font_thickness
-	
-	path = BACKGROUND_IMG
-	background = cv2.imread(path)
-	background = cv2.cvtColor(background , cv2.COLOR_BGR2RGB)
-	height, width, channel = background.shape
-	
-	font = cv2.FONT_HERSHEY_SIMPLEX
-	org = (50, 50)
-	font_size = 1
-	font_color = (255, 255, 255)
-	font_thickness = 2
-	
-	frames = []
-	for i,jk in enumerate(joke10):
-
-		text = str(i) + '\n' + jk[0] + '\n' + jk[1]
-		textsize = cv2.getTextSize('O', font, font_size, font_thickness)[0]
-		gap = textsize[1] + 10
-		wrapped_text = textwrap.wrap(text, width=width//textsize[0])
-
-		image = background.copy()
-
-		for i, line in enumerate(wrapped_text):
-			textsize = cv2.getTextSize(line, font, font_size, font_thickness)[0]
-			y = (height - textsize[1]) // 2 + i * gap
-			x = (width - textsize[0])//2
-			org = (x,y)
-			image = cv2.putText( image , line, org, font, font_size, font_color, font_thickness, cv2.LINE_AA)
-
-#		cv2.imshow('hehe', image)
-#		cv2.waitKey(0)
-		np_img = np.asarray(image)
-		frames.append(np_img)
-
-	get_frame = GetFrame(frames, duration = DURATION)
-	clip = mpy.VideoClip(get_frame, duration=DURATION)
-	clip.write_videofile('joke10.mp4', fps=12)
-	return clip
 
 def get_audio():
-	pass
+    print('making speech')
+    told_jokes, synthesizer = tell_jokes(joke10_plus)
 
+    speeches = []
+    for wav in told_jokes:
+        with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as fp:
+            # todo : find a way to not persist file
+            synthesizer.save_wav(wav, fp)
+            speeches.append(fp.name)
+    told_jokes = speeches
+
+    print(told_jokes)
+
+    audioclips = [mpy.AudioFileClip(i) for i in told_jokes]
+    audioclip = mpy.concatenate_audioclips(audioclips)
+    durations = [clip.duration for clip in audioclips]
+
+    return audioclip, durations
+
+
+def get_video():
+    global background, npimg, joke10, frames, text, wrapped_text
+    global font, org, font_size, font_color, font_thickness
+
+    path = BACKGROUND_IMG
+    background = cv2.imread(path)
+    background = cv2.cvtColor(background, cv2.COLOR_BGR2RGB)
+    height, width, channel = background.shape
+
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    org = (50, 50)
+    font_size = 1
+    font_color = (255, 255, 255)
+    font_thickness = 2
+
+    frames = []
+    for i, jk in enumerate(joke10):
+
+        text = str(i) + '\n' + jk[0] + '\n' + jk[1]
+        textsize = cv2.getTextSize('O', font, font_size, font_thickness)[0]
+        gap = textsize[1] + 10
+        wrapped_text = textwrap.wrap(text, width=width//textsize[0])
+
+        image = background.copy()
+
+        for i, line in enumerate(wrapped_text):
+            textsize = cv2.getTextSize(
+                line, font, font_size, font_thickness)[0]
+            y = (height - textsize[1]) // 2 + i * gap
+            x = (width - textsize[0])//2
+            org = (x, y)
+            image = cv2.putText(image, line, org, font, font_size,
+                                font_color, font_thickness, cv2.LINE_AA)
+
+        np_img = np.asarray(image)
+        frames.append(np_img)
+
+    get_frame = GetFrame(frames, durations=durations)
+    clip = mpy.VideoClip(get_frame, duration=sum(durations))
+    return clip
+
+
+###MAIN###
 joke10 = load_joke10()
-#clip = get_video()
-audio = get_audio()
+joke10_plus = [i + ' ' + j for i, j in joke10]
+
+audioclip, durations = get_audio()
+videoclip = get_video()
+
+videoclip.audio = audioclip
+videoclip.write_videofile('joke10.mp4', fps=12)
